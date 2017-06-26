a5bf0fbe12d5c239aee43f206a6db1bfc42106b4
bijanfallah <bijan.fallah@gmail.com>
2017-06-26 15:04:04 +0200

    ploting correction

--- src/RMSE_std_error.py
+++ src/RMSE_std_error.py
@@ -2,8 +2,8 @@
 Program to plot the changes of RMSE with respect to the changes in correlation length and number of observations
 '''
 # ============================== NAMELIST ======================================
-MAX_L=60
-month_length=30
+
+month_length=10
 SEAS1="JJA"
 SEAS2="DJF"
 NN=500
@@ -13,12 +13,27 @@ M=50 #Number of influential points
 DIR1='/daten/cady/DATA_ASSIMILATION_TESTS_JJA/OI_CCLM/src/test/1.0/20_'+SEAS1+'/'
 DIR2='/daten/cady/DATA_ASSIMILATION_TESTS/OI_CCLM/src/test/1.0/20_'+SEAS2+'/'
 VAR='T_2M'
+buffer = 20
 # ===============================================================================
 
 pdf_name= '/daten/cady/DATA_ASSIMILATION_TESTS/RMSE_L_'+ '_' + str(month_length) + '_' + str(NN) + '_' + str(M) + '_' + '.pdf'
 import numpy as np
 import csv
 import os
+import os.path
+from RMSE_MAPS_INGO import read_data_from_mistral as rdfm
+
+from itertools import izip
+
+
+from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
+import numpy as np
+import cartopy.crs as ccrs
+from CCLM_OUTS import Plot_CCLM
+from RMSE_MAPS_INGO import read_data_from_mistral as rdfm
+MAX_L=len(np.arange(0.10,5.01,0.05))
+import pandas as pd
 #res= np.zeros((16,50))
 #res= np.zeros((7,50))
 res1= np.zeros((7,MAX_L))
@@ -28,7 +43,7 @@ k=0
 
 for i in range(500,600,100):
     kk=0
-    for j in np.arange(0.10,2.1,0.1):
+    for j in np.arange(0.10,5.01,0.05):
     #for j in ["0,1" ,  "0,4",  "0,7",  "1,0" ,  "1,3",  "1,6",  "1,9",  "2,11",  "2,41",  "2,71","3,1"]:
     #for j in ["0,01" ,  "0,06",  "0,11",  "0,16" ,  "0,21",  "0,26",  "0,31",  "0,36",  "0,41",  "0,46"]:
     #for j in ["0,1",  "0,2",  "0,3",  "0,4",  "0,5",  "0,6",  "0,7",  "0,8",  "0,9",  "1,0" ,  "1,1", "1,2",  "1,3",  "1,4",  "1,5",  "1,6",  "1,7",  "1,8",  "1,9",  "2,0" ,  "2,1",  "2,2", "2,3",  "2,4",  "2,5",  "2,6",  "2,7",  "2,8",  "2,9",  "3,0" ,  "3,1",  "3,2",  "3,3", "3,4",  "3,5",
@@ -37,17 +52,75 @@ for i in range(500,600,100):
      #         "5,6",  "5,7",  "5,8",  "5,9",  "6,0" ]:
         
         #names = 'TEMP/RMSE_last_m_' + SEAS + '_' + str(M) + '_' + str(j) + '_' + str(i) + '.pdf.csv'
+        
         names1 = DIR1 + 'test01_' + str(j) + '0_1.7_' +  str(i) + '_19_1.0_20/Trash/RMSE_RMSE_ANALYSIS_' + SEAS1 + '_' + str(M) + '_1.7' + '_' + str(i) + '_'+ VAR + '_' +'19.pdf19.csv'
+        if os.path.exists(names1):
+		result1 = np.array(list(csv.reader(open(names1, "rb"), delimiter=','))).astype('float')
+	else:
+                names1 = DIR1 + 'test01_' + str(j) + '_1.7_' +  str(i) + '_19_1.0_20/Trash/RMSE_RMSE_ANALYSIS_' + SEAS1 + '_' + str(M) + '_1.7' + '_' + str(i) + '_'+ VAR + '_' +'19.pdf19.csv'
+                result1 = np.array(list(csv.reader(open(names1, "rb"), delimiter=','))).astype('float') 
+			
+			
+	names2 = DIR2 + 'test01_' + str(j) + '0_2.1_' +  str(i) + '_19_1.0_20/Trash/RMSE_RMSE_ANALYSIS_' + SEAS2 + '_' + str(M) + '_2.1'  + '_' + str(i) + '_'+ VAR + '_' +'19.pdf19.csv' 
+	if os.path.exists(names2):
+		result2 = np.array(list(csv.reader(open(names2, "rb"), delimiter=','))).astype('float')
+	else:
+                names2 = DIR2 + 'test01_' + str(j) + '_2.1_' +  str(i) + '_19_1.0_20/Trash/RMSE_RMSE_ANALYSIS_' + SEAS2 + '_' + str(M) + '_2.1'  + '_' + str(i) + '_'+ VAR + '_' +'19.pdf19.csv' 
+                result2 = np.array(list(csv.reader(open(names2, "rb"), delimiter=','))).astype('float')
+		    
+			
         
-        names2 = DIR2 + 'test01_' + str(j) + '0_2.1_' +  str(i) + '_19_1.0_20/Trash/RMSE_RMSE_ANALYSIS_' + SEAS2 + '_' + str(M) + '_2.1'  + '_' + str(i) + '_'+ VAR + '_' +'19.pdf19.csv' 
         
-        result1 = np.array(list(csv.reader(open(names1, "rb"), delimiter=','))).astype('float')
-	result2 = np.array(list(csv.reader(open(names2, "rb"), delimiter=','))).astype('float')
+	
         res1[k,kk]=result1[0,1]
 	res2[k,kk]=result2[0,1]
         
         kk=kk+1
     k=k+1
+
+# ----- here read and import the forecast RMSE for JJA and DJF ----
+
+Forecast_3_JJA = np.array(pd.read_csv(DIR1+'test01_5.00_1.7_500_19_1.0_20'+'/'+'Trash/SEASON_MEAN1' + '_' + SEAS1 + '.csv', header=None))#Reading the Forecast values JJA
+Forecast_3_DJF = np.array(pd.read_csv(DIR2+'test01_5.00_2.1_500_19_1.0_20'+'/'+'Trash/SEASON_MEAN1' + '_' + SEAS2 + '.csv', header=None))#Reading the Forecast values DJF
+t_f_JJA = np.zeros((month_length,Forecast_3_JJA.shape[0],Forecast_3_JJA.shape[1]))
+t_f_DJF = t_f_JJA-t_f_JJA
+for month in range(0, month_length):# Reading the ensemble forecast for each month!
+    t_f_JJA[month,:,:] = pd.read_csv(DIR1+'test01_5.00_1.7_500_19_1.0_20'+'/'+'Trash/SEASON_MEAN' + str(month) + '_' + SEAS1 + '.csv', header=None)
+    t_f_DJF[month,:,:] = pd.read_csv(DIR2+'test01_5.00_2.1_500_19_1.0_20'+'/'+'Trash/SEASON_MEAN' + str(month) + '_' + SEAS2 + '.csv', header=None)
+t_f_JJA = np.array(t_f_JJA)
+t_f_DJF = np.array(t_f_DJF)
+
+name_1 = 'member_relax_3_big_00_' + VAR + '_ts_splitseas_1990_1999_' + SEAS1 + '.nc'
+t_o_JJA, lat_o, lon_o, rlat_o, rlon_o =rdfm(dir='NETCDFS_CCLM/03/member_relax_3_big_00/post/',
+                                        name=name_1,
+                                        var=VAR)
+
+name_2 = 'member_relax_3_big_00_' + VAR + '_ts_splitseas_1990_1999_' + SEAS2 + '.nc'
+t_o_DJF, lat_o, lon_o, rlat_o, rlon_o =rdfm(dir='NETCDFS_CCLM/03/member_relax_3_big_00/post/',
+                                        name=name_2,
+                                        var=VAR)
+
+dext_lon = t_o_DJF.shape[2] - (2 * buffer)
+dext_lat = t_o_DJF.shape[1] - (2 * buffer)
+start_lon=(buffer+4)
+start_lat=(buffer-4)
+
+obs_JJA = t_o_JJA[0:10, buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+RMSE_TIME_SERIES_Forecast_JJA=np.zeros(obs_JJA.shape[0])
+obs_DJF = t_o_DJF[0:10, buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+RMSE_TIME_SERIES_Forecast_DJF=np.zeros(obs_DJF.shape[0])
+
+for i in range(0,obs_JJA.shape[0]):
+    print i	
+    forecast_orig_ts_JJA=np.squeeze(t_f_JJA[i,:,:])
+    obs_resh_ts_JJA=np.squeeze(obs_JJA[i,:,:])
+    RMSE_TIME_SERIES_Forecast_JJA[i] = mean_squared_error(obs_resh_ts_JJA, forecast_orig_ts_JJA) ** 0.5 #Calculating RMSEs for each month for forecast
+    forecast_orig_ts_DJF=np.squeeze(t_f_DJF[i,:,:])
+    obs_resh_ts_DJF=np.squeeze(obs_DJF[i,:,:])
+    RMSE_TIME_SERIES_Forecast_DJF[i] = mean_squared_error(obs_resh_ts_DJF, forecast_orig_ts_DJF) ** 0.5
+    	
+   # print np.max(obs_resh_ts[:,1]),np.max(forecast_orig_ts_JJA[:,1])
+# ----- ------------------------------------------------------ ----
 
 
 
@@ -56,13 +129,17 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 fig.set_size_inches(14, 10)
 #for i in range(16):
-x=np.arange(.10,2.1,.1)
+x=np.arange(.10,5.01,.05)
 i=0
 #for i in range(7):
 #    ax.plot(x,res[i,:],'o-', label=str(i*100+500), lw=3, alpha=.7, ms=10)
 #    ax.legend(loc='upper center', shadow=True)
 ax.plot(x,res1[i,],'rd-', label=str("JJA"), lw=3, ms=10, alpha=.5)
+ax.plot(6,np.mean(RMSE_TIME_SERIES_Forecast_JJA),'kd')
+print RMSE_TIME_SERIES_Forecast_JJA
 ax.plot(x,res2[i,],'bo-', label=str("DJF"), lw=3, ms=10, alpha=.5)
+ax.plot(6,np.mean(RMSE_TIME_SERIES_Forecast_DJF),'ko')
+#ax.boxplot(RMSE_TIME_SERIES_Forecast_DJF)
 plt.legend(loc=4, shadow=False,fontsize=32)
 #if SEAS == 'DJF':
 #ax.plot(x[16],min(res1[0,:]),'r*', label=str(500), lw=3,  ms=20) # JJA
@@ -76,6 +153,7 @@ ax.set_ylabel(r"$RMSE$", labelpad=5,size=32)
 ax.set_xlabel(r"$L$", labelpad=5,size=32)
 
 plt.xlim(0,(6.2))
+plt.ylim(0.12,(0.2))
 #if SEAS == 'DJF':
  #   plt.ylim(0.6,1.4) #DJF
 #else:
--- src/loop_obs_error_io_script.sh
+++ src/loop_obs_error_io_script.sh
@@ -6,7 +6,7 @@ set -ex
 here=$(pwd)
 #while [ $NOO -lt 31 ]; do
 
-for i in $(seq 0.1 0.05 2); do
+for i in $(seq 0.1 0.05 5); do
     sed -i "s/std_err=666/std_err=$i/g" ${here}/io_scrpt.sh
     ./io_scrpt.sh
     sed -i "s/std_err=$i/std_err=666/g" ${here}/io_scrpt.sh
