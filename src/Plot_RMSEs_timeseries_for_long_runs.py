81dec22e1650f0343c5566df8972e5617db2c2b4
bijanfallah <bijan.fallah@gmail.com>
2017-07-13 11:27:13 +0200

    some corrections

--- src/Plot_RMSEs_timeseries_for_long_runs.py
+++ src/Plot_RMSEs_timeseries_for_long_runs.py
@@ -15,30 +15,30 @@ NN=500
 SEAS='JJA'
 Vari   = 'T_2M'
 #Vari   = 'TOT_PREC'
-month_length=30   # number of the seasons (years)
+month_length=36   # number of the seasons (years)
 start_time=0
 here = "."+"/"
 no_members=4
 buffer=20
 
 # -----------------------------------------------------------------------------
-name_2 = 'member_relax_3_big_00_' + Vari + '_ts_splitseas_1979_2010_' + SEAS + '.nc'
+name_2 = 'member_relax_3_big_00_' + Vari + '_ts_splitseas_1979_2015_' + SEAS + '.nc'
 t_o, lat_o, lon_o, rlat_o, rlon_o =rdfm(dir='NETCDFS_CCLM/03/member_relax_3_big_00/post/', # the observation (default run without shifting)
                                             name=name_2,
                                             var=Vari)
-name_2 = 'member_relax_1_big_04_' + Vari + '_ts_splitseas_1979_2010_' + SEAS + '.nc'
+name_2 = 'member_relax_1_big_04_' + Vari + '_ts_splitseas_1979_2015_' + SEAS + '.nc'
 t_1, lat_1, lon_1, rlat_1, rlon_1 =rdfm(dir='NETCDFS_CCLM/01/member_relax_1_big_04/post/', 
                                             name=name_2,
                                             var=Vari)
-name_2 = 'member_relax_2_big_04_' + Vari + '_ts_splitseas_1979_2010_' + SEAS + '.nc'
+name_2 = 'member_relax_2_big_04_' + Vari + '_ts_splitseas_1979_2015_' + SEAS + '.nc'
 t_2, lat_2, lon_2, rlat_2, rlon_2 =rdfm(dir='NETCDFS_CCLM/02/member_relax_2_big_04/post/', 
                                             name=name_2,
                                             var=Vari)
-name_2 = 'member_relax_3_big_04_' + Vari + '_ts_splitseas_1979_2010_' + SEAS + '.nc'
+name_2 = 'member_relax_3_big_04_' + Vari + '_ts_splitseas_1979_2015_' + SEAS + '.nc'
 t_3, lat_3, lon_3, rlat_3, rlon_3 =rdfm(dir='NETCDFS_CCLM/03/member_relax_3_big_04/post/', 
                                             name=name_2,
                                             var=Vari)
-name_2 = 'member_relax_4_big_04_' + Vari + '_ts_splitseas_1979_2010_' + SEAS + '.nc'
+name_2 = 'member_relax_4_big_04_' + Vari + '_ts_splitseas_1979_2015_' + SEAS + '.nc'
 t_4, lat_4, lon_4, rlat_4, rlon_4 =rdfm(dir='NETCDFS_CCLM/04/member_relax_4_big_04/post/', 
                                             name=name_2,
                                             var=Vari)
@@ -68,11 +68,22 @@ print buffer, t_o.shape, t_1.shape
 d0 = t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon].shape[0]
 d1 = t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon].shape[1]
 d2 = t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon].shape[2]
-time_series[:,0]   =   np.mean(t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon].reshape(d0,d1*d2),axis=1)
-time_series[:,1]   =   np.mean(t_2[:,buffer-4:buffer-4+ dext_lat, buffer-4:buffer-4+ dext_lon].reshape(d0,d1*d2),axis=1)
-time_series[:,2]   =   np.mean(t_3[:,buffer-4:buffer-4+ dext_lat, buffer+4:buffer+4+ dext_lon].reshape(d0,d1*d2),axis=1)
-time_series[:,3]   =   np.mean(t_4[:,buffer+4:buffer+4+ dext_lat, buffer+4:buffer+4+ dext_lon].reshape(d0,d1*d2),axis=1)
-time_series_Nature = np.mean(t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon].reshape(d0,d1*d2),axis=1)
+
+t_1 = t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon] #- t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+#time_series[:,0]   =   np.mean(t_1[:,buffer+4:buffer+4+ dext_lat, buffer-4:buffer-4+ dext_lon].reshape(d0,d1*d2),axis=1)
+time_series[:,0]   =   np.mean(t_1.reshape(d0,d1*d2),axis=1)
+t_2 = t_2[:,buffer-4:buffer-4+ dext_lat, buffer-4:buffer-4+ dext_lon] # - t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+time_series[:,1]   =   np.mean(t_2.reshape(d0,d1*d2),axis=1)
+t_3 = t_3[:,buffer-4:buffer-4+ dext_lat, buffer+4:buffer+4+ dext_lon] #- t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+time_series[:,2]   =   np.mean(t_3.reshape(d0,d1*d2),axis=1)
+t_4 = t_4[:,buffer+4:buffer+4+ dext_lat, buffer+4:buffer+4+ dext_lon] #- t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+time_series[:,3]   =   np.mean(t_4.reshape(d0,d1*d2),axis=1)
+t_mean = (t_1 + t_2 + t_3 + t_4)/4
+t_o = t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon]
+#time_series[:,1]   =   np.mean(t_2[:,buffer-4:buffer-4+ dext_lat, buffer-4:buffer-4+ dext_lon].reshape(d0,d1*d2),axis=1)
+#time_series[:,2]   =   np.mean(t_3[:,buffer-4:buffer-4+ dext_lat, buffer+4:buffer+4+ dext_lon].reshape(d0,d1*d2),axis=1)
+#time_series[:,3]   =   np.mean(t_4[:,buffer+4:buffer+4+ dext_lat, buffer+4:buffer+4+ dext_lon].reshape(d0,d1*d2),axis=1)
+#time_series_Nature = np.mean(t_o[:,buffer:buffer + dext_lat, buffer:buffer + dext_lon].reshape(d0,d1*d2),axis=1)
 time = range(1,month_length +1)
 time_series_mean = np.mean(time_series,1)
 time_series_max  = np.max(time_series,1) 
@@ -85,7 +96,7 @@ fig.set_size_inches(14, 10)
 plt.fill_between(time, time_series_max,
                  time_series_min, color='darkgray') #color="#3F5D7D")
 plt.plot(time, time_series_mean, color="black", lw=2, alpha=0.7)
-plt.plot(time, time_series_Nature, color="black",linestyle='--',dashes=(5, 2), lw=2)
+#plt.plot(time, time_series_Nature, color="black",linestyle='--',dashes=(5, 2), lw=2)
 plt.xlabel("Time", fontsize=30,fontname="Times New Roman")
 plt.ylabel(Vari, fontsize=30,fontname="Times New Roman")
 plt.tick_params(axis='both', which='major', labelsize=10)
@@ -93,3 +104,38 @@ xlim([.5,month_length+0.5])
 #fig.set_xticklabels(tick_labels.astype(int))
 plt.savefig(here+''+"Time_series_"+Vari+'_'+SEAS+".pdf", bbox_inches="tight");
 plt.close()
+RMSE=np.zeros((t_o.shape[1],t_o.shape[2]))
+RMSE_time = np.zeros((4,t_o.shape[0]))
+print RMSE_time.shape
+# -------------plot rmse of ensemble mean -------------------------------------
+from sklearn.metrics import mean_squared_error
+for ii in range(0,t_o.shape[0]):
+
+    forecast_resh=np.reshape(t_1[ii,:,:],(1,d1*d2))
+    obs_resh=np.reshape(t_o[ii,:,:],(1,d1*d2))
+    RMSE_time[0,ii] = mean_squared_error(obs_resh, forecast_resh) ** 0.5
+    forecast_resh=np.reshape(t_2[ii,:,:],(1,d1*d2))
+    RMSE_time[1,ii] = mean_squared_error(obs_resh, forecast_resh) ** 0.5
+    forecast_resh=np.reshape(t_3[ii,:,:],(1,d1*d2))
+    RMSE_time[2,ii] = mean_squared_error(obs_resh, forecast_resh) ** 0.5
+    forecast_resh=np.reshape(t_4[ii,:,:],(1,d1*d2))
+    RMSE_time[3,ii] = mean_squared_error(obs_resh, forecast_resh) ** 0.5
+
+		
+time_series_mean = np.mean(RMSE_time,0)
+time_series_max  = np.max(RMSE_time,0) 
+time_series_min  = np.min(RMSE_time,0)
+
+
+fig = plt.figure(222)
+fig.set_size_inches(14, 10)
+plt.fill_between(time, time_series_max, time_series_min, color='darkgray')  
+plt.plot(time, time_series_mean, color="black", lw=2, alpha=0.7)
+plt.xlabel("Time", fontsize=30,fontname="Times New Roman")
+plt.ylabel("RMSE", fontsize=30,fontname="Times New Roman")
+plt.tick_params(axis='both', which='major', labelsize=10)
+xlim([0,month_length+1])
+plt.savefig(here+''+"RMSE_Time_series_"+Vari+'_'+SEAS+".pdf", bbox_inches="tight");
+plt.close()
+
+
